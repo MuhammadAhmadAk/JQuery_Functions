d75291bd954d5d7cea118bbc00b95e9e95d79b24
Ahmad <muhammadahmad9304@gmail.com>
2022-05-07 14:25:00 +0500

    this wala function

---
+++ this.js
@@ -0,0 +1,9 @@
+<script >
+        $(document).ready(function () {
+            $("#btn").click(function () {
+                $(this).hide();
+                console.log("Div is Hide");
+            })
+        })
+    </script>
+    // alag alag div ko hide krta hai jis ko click kry gy wo hide hoga
